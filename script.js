568b234b11f8795c84a67ac7e8fa748541e563c4
jvrabelo <106701350+jvrabelo@users.noreply.github.com>
2024-06-19 14:43:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,77 @@
 
-<script src="script.js"></script>
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
+
 const perguntas = [
     {
-        texto: "Assim que terminou a temporada, o corinthians planeja fazer uma reformulação no elenco , então ele está pensando em contratar dois meias, Quais contratar?",
-        afirmação: [
-            "igor Coronado",
-            "Rodrigo Garro"
+        enunciado: "Já marcou sua sobrancelha?",
+        alternativas: [
+            {
+                texto: "Sim!",
+                afirmacao: "afirmação"
+            },
+            {
+                texto: "Ainda não!",
+                afirmacao: "afirmação"
+            }
         ]
     },
     {
-          texto: "Com a divida do Corinthians, ele planeja diminuir a divida com a entradado novo presidente, quem seria um bom nome para a presidencia do Corinthians?",
-          afirmação: [
-            "augusto Melo",
-            "André Negão"
+        enunciado: "Qual modelo de sobrancelha vc acha melhor?",
+        alternativas: [
+            {
+                texto: "Arqueada.",
+                afirmacao: "afirmação"
+            },
+            {
+                texto: "Curvada.",
+                afirmacao: "afirmação"
+            }
         ]
     },
     {
-      texto: "com a saída do Cassio do Corinthians, o timão busca colocar um goleiro do elenco corintiano,qual seria um bom nome? "
-    ",
-          afirmação: [
-            "Carlos Miguel",
-            "Felipe Longo"
+        enunciado: "Qual método vc prefere?",
+        alternativas: [
+            {
+                texto: "Pinça.",
+                afirmacao: "afirmação"
+            },
+            {
+                texto: "Cera.",
+                afirmacao: "afirmação"
+            }
         ]
     },
     {
-          texto: "Com o fim do Paulistão, o corinthians busca melhorar seu elenco, e com isso busca um novo treinador, Qual seria o melhor nome?",
-          afirmação: [
-            "Antonio Oliveira",
-            "Mano Menezes"
+        enunciado: "Qual procedimento vc acha melhor?",
+        alternativas: [
+            {
+                texto: "Henna.",
+                afirmacao: "afirmação"
+            },
+            {
+                texto: "micropigmentação.",
+                afirmacao: "afirmação"
+            }
         ]
     },
     {
-          texto: "O corinthians buca um titulo que ainda é inedito na sua enorme pratileira de troféus, e com isso e ele ganhar esse titulo, ele vai se consagrar campeão de tudo, Qual titulo que falta?",
-          afirmação: [
-            "Sudamericana",
-            "Libertadores"
+        enunciado: "Como vc prefere a henna? ",
+        alternativas: [
+            {
+                texto: "Natural.",
+                afirmacao: "afirmação"
+            },
+            {
+                texto: "Marcada.",
+                afirmacao: "afirmação"
+            }
         ]
-    }
-]
+    },
+];
 
 let atual = 0;
 let perguntaAtual;
@@ -53,17 +82,21 @@ function mostraPergunta() {
     caixaPerguntas.textContent = perguntaAtual.enunciado;
     mostraAlternativas();
 }
-function mostraAlternativas(){
-    for(const alternativa of perguntaAtual.alternativas){
-            const botaoAlternativas = document.createElement("button");
-            botaoAlternativas.textContent = alternativa.texto;
-            botaoAlternativas.addEventListener("click",() => respostaSelecionada(alternativa));
-            caixaAlternativas.appendChild(botaoAlternativas);   
-           
 
-function respostaSelecionada(opcaoSelecionada) {
-    const afirmacoes = opcaoSelecionada.afirmacao;
-    historia += afirmacoes + "";
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacoes = opcaoSelecionada.afirmacoes;
+    historiaFinal = afirmacoes;
     atual++;
     mostraPergunta();
 }
+
+mostraPergunta();
